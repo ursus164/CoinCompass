498557796f69349ee76dd92726ebb18afa287745
ursus164 <Galawieprzowa@gmail.com>
2024-01-05 22:09:42 +0100

    Poprawki, utworzenie javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.data.api"},{"l":"com.data.coin"},{"l":"com.data.currency"},{"l":"com.data.gui"},{"l":"com.data.market"}];updateSearchResults();
